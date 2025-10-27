d7deb697592f3406604542bc973dc46af4c06337
jonahb023 <134671079+jonahb023@users.noreply.github.com>
2025-10-26 22:17:17 -0500

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,20 @@
+//Author: Jonah Beck
+//Assignment Title: Hello World
+//Assignment Description: Prints "Hola Mundo :-)"
+//Due Date: 8/23
+//Date Created: 8/22
+//Date Last Modified: 8/22
+
+#include <iostream>
+using namespace std;
+    int main(){
+
+        cout << "Hola Mundo. :-)";
+
+        return 0;
+    }
+
+//Data Abstraction: 
+//Input: 
+//Process:
+//Output: "Hola Mundo. :-)"
